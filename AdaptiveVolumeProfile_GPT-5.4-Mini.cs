1b5bc0a5f257fdac4d05786df3059f954f421cc2
luis-avila <wicho696@gmail.com>
2026-03-23 21:58:15 -0500

    Add AdaptiveVolumeProfile placeholder pending NT8 KB verification

---
+++ AdaptiveVolumeProfile_GPT-5.4-Mini.cs
@@ -0,0 +1,16 @@
+#region Using declarations
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Windows.Media;
+using NinjaTrader.Gui.Tools;
+using NinjaTrader.NinjaScript;
+using NinjaTrader.NinjaScript.Indicators;
+using SharpDX;
+using SharpDX.Direct2D1;
+#endregion
+
+// This file intentionally not finalized because required NT8 knowledge base files
+// were not available in the verified Chroma collection during this session.
